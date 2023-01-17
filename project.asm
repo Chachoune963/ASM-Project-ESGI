7d853867a60668ca284feab723316a112e72d0b6
dudus46 <victorstec01@gmail.com>
2023-01-17 18:34:03 +0100

    Coordy/x bug

--- project.asm
+++ project.asm
@@ -1,4 +1,36 @@
+; external functions from X11 library
+extern XOpenDisplay
+extern XDisplayName
+extern XCloseDisplay
+extern XCreateSimpleWindow
+extern XMapWindow
+extern XRootWindow
+extern XSelectInput
+extern XFlush
+extern XCreateGC
+extern XSetForeground
+extern XDrawLine
+extern XDrawPoint
+extern XFillArc
+extern XNextEvent
+
+; external functions from stdio library (ld-linux-x86-64.so.2)    
 extern printf
+extern exit
+
+%define	StructureNotifyMask	131072
+%define KeyPressMask		1
+%define ButtonPressMask		4
+%define MapNotify		19
+%define KeyPress		2
+%define ButtonPress		4
+%define Expose			12
+%define ConfigureNotify		22
+%define CreateNotify 16
+%define QWORD	8
+%define DWORD	4
+%define WORD	2
+%define BYTE	1
 
 global main
 
@@ -7,11 +39,27 @@ global main
 %DEFINE MAX_Y 255
 
 section .data
+event:		times	24 dq 0
+
+x1:	dd	0
+x2:	dd	0
+y1:	dd	0
+y2:	dd	0
+
 fmt_printf: db "%d", 10, 0
 fmt_index: db "vs: %d", 10, 0
 fmt_min: db "Minimum: %d", 10, 0
 
 section .bss
+display_name:	resq	1
+screen:			resd	1
+depth:         	resd	1
+connection:    	resd	1
+width:         	resd	1
+height:        	resd	1
+window:		resq	1
+gc:		resq	1
+
 coordx: resw NUM_POINTS
 coordy: resw NUM_POINTS
 enveloppe: resw NUM_POINTS
@@ -44,7 +92,8 @@ populatex:
     inc rbx
     cmp rbx, NUM_POINTS
     jb populatex
-
+    
+mov rbx, 0
 populatey:
     stc
     checkcf:
@@ -57,6 +106,13 @@ populatey:
         jae mody
     mov ax, word[randnum]
     mov word[coordy+rbx*2], ax
+    ;########
+    mov rdi, fmt_printf
+    movzx rsi, word[coordy+rbx*2]
+    mov rax, 0
+    call printf
+    ;########
+    
     inc rbx
     cmp rbx, NUM_POINTS
     jb populatey
@@ -82,14 +138,141 @@ minAlgo:
     inc rbx
     cmp rbx, NUM_POINTS
     jb minAlgo
+    
+    xor     rdi,rdi
+call    XOpenDisplay	; Création de display
+mov     qword[display_name],rax	; rax=nom du display
+
+; display_name structure
+; screen = DefaultScreen(display_name);
+mov     rax,qword[display_name]
+mov     eax,dword[rax+0xe0]
+mov     dword[screen],eax
+
+mov rdi,qword[display_name]
+mov esi,dword[screen]
+call XRootWindow
+mov rbx,rax
+
+mov rdi,qword[display_name]
+mov rsi,rbx
+mov rdx,10
+mov rcx,10
+mov r8,400	; largeur
+mov r9,400	; hauteur
+push 0xFFFFFF	; background  0xRRGGBB
+push 0x00FF00
+push 1
+call XCreateSimpleWindow
+mov qword[window],rax
+
+mov rdi,qword[display_name]
+mov rsi,qword[window]
+mov rdx,131077 ;131072
+call XSelectInput
+
+mov rdi,qword[display_name]
+mov rsi,qword[window]
+call XMapWindow
+
+mov rsi,qword[window]
+mov rdx,0
+mov rcx,0
+call XCreateGC
+mov qword[gc],rax
+
+mov rdi,qword[display_name]
+mov rsi,qword[gc]
+mov rdx,0x000000	; Couleur du crayon
+call XSetForeground
+
+boucle: ; boucle de gestion des évènements
+mov rdi,qword[display_name]
+mov rsi,event
+call XNextEvent
+
+cmp dword[event],ConfigureNotify	; à l'apparition de la fenêtre
+je dessin			; on saute au label 'dessin'
+
+cmp dword[event],KeyPress			; Si on appuie sur une touche
+je closeDisplay					; on saute au label 'closeDisplay' qui ferme la fenêtre
+jmp boucle
+    
+;#########################################
+;#        DEBUT DE LA ZONE DE DESSIN     #
+;#########################################
+dessin:
+
+    ; boucle dessin point
+    
+    mov rbx, 0  
+    draw_loop:
+        
+        movzx rax, word [coordx+rbx*2]
+        movzx rcx, word [coordy+rbx*2]
+        
+        ;couleur du point 1
+        mov rdi,qword[display_name]
+        mov rsi,qword[gc]
+        mov edx,0xFF0000	; Couleur du crayon ; rouge
+        call XSetForeground
+        
+        ; Dessin d'un point rouge sous forme d'un petit rond : coordonnées (100,200)
+        mov rdi,qword[display_name]
+        mov rsi,qword[window]
+        mov rdx,qword[gc]
+        mov rcx,rax		; coordonnée en x du point
+        sub ecx,3
+        mov r8,rcx 		; coordonnée en y du point
+        sub r8,3
+        mov r9,6
+        mov rax,23040
+        push rax
+        push 0
+        push r9
+        call XFillArc
+        
+        ;#############
+        push rax
+        mov rdi, fmt_printf
+        mov rsi, rax
+        mov rax, 0
+        call printf
+        ;#############
+        
+        pop rax
+        
+        ;++loopcounter
+        inc rbx
+        
+        ;check if end loop
+        cmp rbx, NUM_POINTS
+        jb draw_loop
+
+; ############################
+; # FIN DE LA ZONE DE DESSIN #
+; ############################
+jmp flush
+
+flush:
+mov rdi,qword[display_name]
+call XFlush
+jmp boucle
+mov rax,34
+syscall
+
+closeDisplay:
+    mov     rax,qword[display_name]
+    mov     rdi,rcx
+    call    XCloseDisplay
+    xor	    rdi,rdi
+    call    exit	
 
 ; Une fois cette étape fini, nous connaissons le point le plus à gauche.
 ; On commence donc la marche de Jarvis
 mov rbx, 0
 jarvis:
     
-
-
 ; Pour fermer le programme proprement :
 mov    rax, 60         
 mov    rdi, 0
