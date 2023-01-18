5ee4d9254240266712a340fa2aff406331e15f31
Chachoune963 <sacha@toutut.fr>
2023-01-18 21:20:40 +0100

    Ajout d'un flag si oui ou non a l'interieur sous la forme de 'isInside'

--- project.asm
+++ project.asm
@@ -57,7 +57,7 @@ orientation:
     sub cx, dx
     
     cmp cx, 0
-    jle clockwise
+    jl clockwise
     
     mov eax, 1
     jmp endori
@@ -70,7 +70,7 @@ ret
 
 global main
 
-%DEFINE NUM_POINTS 40
+%DEFINE NUM_POINTS 5
 %DEFINE MAX_X 255
 %DEFINE MAX_Y 255
 
@@ -95,6 +95,8 @@ tuezmoi: db "Depuis le point %d", 10, 0
 env: db "Enveloppe: %d", 10, 0
 coef: db "Coef: %d", 10, 0
 resultat: db "Resultat: %d", 10, 0
+coordx: dw 10, 102, 102, 108, 19
+coordy: dw 171, 3, 182, 103, 25
 
 section .bss
 display_name:	resq	1
@@ -106,8 +108,8 @@ height:        	resd	1
 window:		resq	1
 gc:		resq	1
 
-coordx: resw NUM_POINTS
-coordy: resw NUM_POINTS
+;coordx: resw NUM_POINTS
+;coordy: resw NUM_POINTS
 enveloppe: resw NUM_POINTS
 sizeEnveloppe: resb 1
 randnum: resw 1
@@ -117,42 +119,47 @@ P: resw 1
 Q: resw 1
 I: resw 1
 
+lastPointX: resw 1
+lastPointY: resw 1
+isInside: resb 1
+checkIndex: resw 1
+
 section .text
 
 main:
 ; Génération des points du programme
-mov rbx, 0
-populatex:
-    rdrand ax
-    mov [randnum], ax
-    modx:
-        sub word[randnum], MAX_X
-        cmp word[randnum], MAX_X
-        jae modx
-    mov ax, word[randnum]
-    mov word[coordx+rbx*2], ax
-    
-    inc rbx
-    cmp rbx, NUM_POINTS
-    jb populatex
+;mov rbx, 0
+;populatex:
+;    rdrand ax
+;    mov [randnum], ax
+;    modx:
+;        sub word[randnum], MAX_X
+;        cmp word[randnum], MAX_X
+;        jae modx
+;    mov ax, word[randnum]
+;    mov word[coordx+rbx*2], ax
+;    
+;    inc rbx
+;    cmp rbx, NUM_POINTS
+;    jb populatex
 
-mov rbx, 0
-populatey:
-    stc
-    checkcf:
-        rdrand ax
-    jnc checkcf
-    mov word[randnum], ax
-    mody:
-        sub word[randnum], MAX_Y
-        cmp word[randnum], MAX_Y
-        jae mody
-    mov ax, word[randnum]
-    mov word[coordy+rbx*2], ax
-
-    inc rbx
-    cmp rbx, NUM_POINTS
-    jb populatey
+;mov rbx, 0
+;populatey:
+;    stc
+;    checkcf:
+;        rdrand ax
+;    jnc checkcf
+;    mov word[randnum], ax
+;    mody:
+;        sub word[randnum], MAX_Y
+;        cmp word[randnum], MAX_Y
+;        jae mody
+;    mov ax, word[randnum]
+;    mov word[coordy+rbx*2], ax
+;
+;    inc rbx
+;    cmp rbx, NUM_POINTS
+;    jb populatey
     
 mov rbx, 0
 printloop:
@@ -217,6 +224,9 @@ jarvis:
     nofix:
     mov word[I], 0
     parcoursListe:
+        mov bx, word[P]
+        cmp word[I], bx
+        je nocandid
         ; Coord de P dans di et si
         movzx rax, word[P]
         mov di, word[coordx+rax*2]
@@ -263,8 +273,84 @@ printenv:
 
     inc rbx
     cmp bx, word[sizeEnveloppe]
-    jb printenv    
+    jb printenv  
     
+rdrand ax
+mov word[lastPointX], 50; ax
+lastmodx:
+;    sub word[lastPointX], MAX_X
+;    cmp word[lastPointX], MAX_X
+;    jae lastmodx
+    
+rdrand ax
+mov word[lastPointY], 75; ax
+lastmody:
+;    sub word[lastPointY], MAX_Y
+;    cmp word[lastPointY], MAX_Y
+;    jae lastmody
+    
+mov word[checkIndex], 0
+mov word[isInside], 1
+checkInside:
+    movzx rax, word[checkIndex]
+    mov bx, word[enveloppe+rax*2]
+    mov word[P], bx
+    inc bx
+    cmp word[sizeEnveloppe], bx
+    jne nolooped
+    
+    mov bx, word[enveloppe]
+    mov word[I], bx
+    jmp endlooped
+    
+    nolooped:
+    mov bx, word[enveloppe+(rax+1)*2]
+    mov word[I], bx
+    
+    endlooped:
+    ; Coord de P dans di et si
+    movzx rax, word[P]
+    mov di, word[coordx+rax*2]
+    mov si, word[coordy+rax*2]
+    
+    ; Coord de I dans dx et cx
+    movzx rax, word[I]
+    mov dx, word[coordx+rax*2]
+    mov cx, word[coordy+rax*2]
+    
+    ; Coord de lastPoint dans r8w et r9w
+    mov r8w, word[lastPointX]
+    mov r9w, word[lastPointY]
+    mov rax, 0
+    call orientation
+    
+    cmp eax, 0
+    jne nextCheck
+    
+    mov byte[isInside], 0
+    
+    nextCheck:
+    inc word[checkIndex]
+    movzx rax, word[sizeEnveloppe]
+    cmp word[checkIndex], ax
+    jb checkInside
+
+mov rdi, printx
+movzx rsi, word[lastPointX]
+mov rax, 0
+call printf
+
+mov rdi, printy
+movzx rsi, word[lastPointY]
+mov rax, 0
+call printf
+
+mov rdi, resultat
+movzx rsi, byte[isInside]
+mov rax, 0
+call printf
+        
+        
 ;fenetre dessin   
 xor     rdi,rdi
 call    XOpenDisplay	; Création de display
